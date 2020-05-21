54b85935ca96b02c81d54455182713d3f4483f84
sungg <zwsun1948@163.com>
2020-05-21 16:47:56 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/index.js',
   output: {
-    filename: '[name].[contenthash].js'
+    filename: 'index.[contenthash].js'
+  },
+  plugins: [new HtmlWebpackPlugin(
+    {
+        title: 'webpack-demo',
+        template: 'src/assets/index.html'
+    }
+  )],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader'],
+      },
+    ],
   }
 };
+
