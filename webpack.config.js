afb78a2641b1a41fa3f684727339f06a600dd1d5
sungg <zwsun1948@163.com>
2020-05-21 17:07:09 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,28 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    filename: 'index.[contenthash].js'
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
   },
-  plugins: [new HtmlWebpackPlugin(
-    {
-        title: 'webpack-demo',
-        template: 'src/assets/index.html'
-    }
-  )],
+  entry: "./src/index.js",
+  output: {
+    filename: "index.[contenthash].js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "webpack-demo",
+      template: "src/assets/index.html",
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader','css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
-  }
+  },
 };
-
