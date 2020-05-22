f8d963d568234ab0baba00a0ade132eca80d60ee
sungg <zwsun1948@163.com>
2020-05-22 12:14:21 +0800

    debug

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import x from "./x.js";
 import png from "./assets/1.png";
 
 const div = document.getElementById("app1");
-div.innerHTML = `
-    <img src='${png}'>
-`;
 
 const button = document.createElement("button");
 button.innerText = "懒加载";
